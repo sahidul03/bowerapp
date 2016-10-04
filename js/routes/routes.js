52b293c58f9780e02c6e44f2e3d833f127c49763
sahidulislam <sahidul.islam@rootnext.com>
2016-10-04 20:32:18 +0600

    logout issue fixed

--- js/routes/routes.js
+++ js/routes/routes.js
@@ -132,12 +132,15 @@ bowerApp.config(function($authProvider, $urlRouterProvider, $stateProvider) {
 bowerApp.$inject = ['Car', 'HostServerDomain', 'Upload'];
 
 // create the controller and inject Angular's $scope
-bowerApp.controller('mainController', function($scope,$auth,$rootScope, Car, HostServerDomain) {
+bowerApp.controller('mainController', function($scope,$auth,$rootScope,$state, Car, HostServerDomain) {
 	// create a message to display in our view
 	$scope.message = 'Everyone come and see how good I look!';
 	$scope.hostname = HostServerDomain;
 	$scope.cars = Car.all().then(SuccessFn, ErrorFn);
 
+	$rootScope.$on('auth:logout-success', function(ev) {
+		$state.go('login');
+	});
 
 	function SuccessFn(data) {
 		$scope.cars = data.data;
